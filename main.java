7580eebb1d6c2c66b2f5af184aa6c99e79a37012
Fatimah A F A M Alghannam <83319@kfh.com>
2024-01-03 10:27:57 +0300

    ARRAY_JAVA_CODE

---
+++ main.java
@@ -0,0 +1,21 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+class Main {
+    public static void main(String[] args) {
+  int [] temperatures={23,24,25,26,16};//five temp.
+      temperatures[2]=11;
+      for(int temp:temperatures){
+        System.out.println("Todays Weather:"+ temp);
+      }
+      
+      
+    }
+  
+    // @Test
+    // void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
+  }
+  
